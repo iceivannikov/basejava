91e4d18e209115ebec274f6533a10ea5d386cbbc
Viktor Ivannikov <iceivannikov@gmail.com>
2024-01-13 17:06:29 +0300

    Add to git DataOutputSerializationStrategy class

---
+++ src/com/ivannikov/webapp/storage/serialization/DataOutputSerializationStrategy.java
@@ -0,0 +1,40 @@
+package com.ivannikov.webapp.storage.serialization;
+
+import com.ivannikov.webapp.model.ContactType;
+import com.ivannikov.webapp.model.Resume;
+
+import java.io.*;
+import java.util.Map;
+
+public class DataOutputSerializationStrategy implements Strategy {
+
+    @Override
+    public void doWrite(Resume resume, OutputStream os) throws IOException {
+        try (DataOutputStream dos = new DataOutputStream(os)) {
+            dos.writeUTF(resume.getUuid());
+            dos.writeUTF(resume.getFullName());
+            Map<ContactType, String> contacts = resume.getContacts();
+            dos.writeInt(contacts.size());
+            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
+                dos.writeUTF(entry.getKey().name());
+                dos.writeUTF(entry.getValue());
+            }
+        }
+    }
+
+    @Override
+    public Resume doRead(InputStream is) throws IOException {
+        try (DataInputStream dis = new DataInputStream(is)) {
+            String uuid = dis.readUTF();
+            String fullName = dis.readUTF();
+            Resume resume = new Resume(uuid, fullName);
+            int size = dis.readInt();
+            for (int i = 0; i < size; i++) {
+                ContactType contactType = ContactType.valueOf(dis.readUTF());
+                String value = dis.readUTF();
+                resume.addContact(contactType, value);
+            }
+            return resume;
+        }
+    }
+}
