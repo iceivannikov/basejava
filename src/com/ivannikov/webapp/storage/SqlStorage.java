8d7cdb42d78cc0e70be5c0f3a6cb9428afa363de
Viktor Ivannikov <iceivannikov@gmail.com>
2024-03-02 15:06:31 +0300

    hw_16 refactor SqlStorage and JsonParser classes

--- src/com/ivannikov/webapp/storage/SqlStorage.java
+++ src/com/ivannikov/webapp/storage/SqlStorage.java
@@ -3,6 +3,7 @@ package com.ivannikov.webapp.storage;
 import com.ivannikov.webapp.exception.NotExistStorageException;
 import com.ivannikov.webapp.model.*;
 import com.ivannikov.webapp.sql.SqlHelper;
+import com.ivannikov.webapp.util.JsonParser;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -142,53 +143,14 @@ public class SqlStorage implements Storage {
         });
     }
 
-    private void saveSectionValue(PreparedStatement ps, Map.Entry<SectionType, Section> entry) throws SQLException {
-        switch (entry.getValue()) {
-            case TextSection textSection -> saveTextSection(ps, textSection);
-            case ListSection listSection -> saveListSection(ps, listSection);
-            case OrganizationSection organizationSection -> saveOrganizationSection(ps, organizationSection);
-            default -> throw new IllegalStateException("Unexpected value: " + entry.getValue());
-        }
-    }
-
-    private void saveTextSection(PreparedStatement ps, TextSection textSection) throws SQLException {
-        String string = textSection.getTextSection();
-        ps.setString(3, string);
-    }
-
-    private void saveListSection(PreparedStatement ps, ListSection listSection) throws SQLException {
-        List<String> list = listSection.getListSections();
-        String join = String.join("\n", list);
-        ps.setString(3, join);
-    }
-
-    private void saveOrganizationSection(PreparedStatement ps, OrganizationSection organizationSection) throws SQLException {
-        List<Organization> organizations = organizationSection.getOrganizations();
-        List<String> org = new ArrayList<>();
-        String join = null;
-        for (Organization organization : organizations) {
-            org.add(organization.getName());
-            org.add(organization.getWebsite());
-            List<Organization.Period> periods = organization.getPeriods();
-            for (Organization.Period period : periods) {
-                org.add(period.getName());
-                org.add(period.getDescription());
-                org.add(period.getStartDate().toString());
-                org.add(period.getEndDate().toString());
-            }
-            join = String.join("\n", org);
-        }
-        System.out.println(join);
-        ps.setString(3, join);
-    }
-
     private void insertSection(Resume resume, Connection conn) throws SQLException {
         try (PreparedStatement ps = conn.prepareStatement("INSERT INTO section (resume_uuid, type, value) VALUES (?,?,?)")) {
             Map<SectionType, Section> sections = resume.getSections();
             for (Map.Entry<SectionType, Section> entry : sections.entrySet()) {
                 ps.setString(1, resume.getUuid());
                 ps.setString(2, entry.getKey().name());
-                saveSectionValue(ps, entry);
+                Section section = entry.getValue();
+                ps.setString(3, JsonParser.write(section, Section.class));
                 ps.addBatch();
             }
             ps.executeBatch();
@@ -215,6 +177,14 @@ public class SqlStorage implements Storage {
         }
     }
 
+    private void addSection(ResultSet rs, Resume resume) throws SQLException {
+        String value = rs.getString("value");
+        if (value != null) {
+            SectionType type = SectionType.valueOf(rs.getString("type"));
+            resume.addSection(type, JsonParser.read(value, Section.class));
+        }
+    }
+
     private Resume createResume(String uuid) {
         return sqlHelper.getPreparedStatement("SELECT * FROM resume r WHERE r.uuid=?", ps -> {
             ps.setString(1, uuid);
@@ -224,48 +194,5 @@ public class SqlStorage implements Storage {
             }
             return new Resume(rs.getString("uuid"), rs.getString("full_name"));
         });
-    }
-
-    private void addSection(ResultSet rs, Resume resume) throws SQLException {
-        SectionType type = SectionType.valueOf(rs.getString("type"));
-        Section section = getSection(rs, type);
-        resume.addSection(type, section);
-    }
-
-    private Section getSection(ResultSet rs, SectionType type) throws SQLException {
-        return switch (type) {
-            case PERSONAL, OBJECTIVE -> getTextSection(rs);
-            case ACHIEVEMENT, QUALIFICATIONS -> getListSection(rs);
-            case EXPERIENCE, EDUCATION -> getOrgSection(rs);
-        };
-    }
-
-    private TextSection getTextSection(ResultSet rs) throws SQLException {
-        return new TextSection(rs.getString("value"));
-    }
-
-    private ListSection getListSection(ResultSet rs) throws SQLException {
-        String value = rs.getString("value");
-        String[] split = value.split("\n");
-        List<String> list = new ArrayList<>(Arrays.asList(split));
-        return new ListSection(list);
-    }
-
-    private OrganizationSection getOrgSection(ResultSet rs) throws SQLException {
-        String value = rs.getString("value");
-        String[] split = value.split("\n");
-        List<Organization> organizations = new ArrayList<>();
-        List<Organization.Period> periods = new ArrayList<>();
-        for (String string : split) {
-//            Organization organization = new Organization();
-//            organizations.add(organization.setName(string));
-//            organization.setWebsite(string);
-//            Organization.Period period = new Organization.Period();
-//            period.setName(string);
-//            period.setDescription(string);
-//            period.setStartDate(LocalDate.parse(string));
-//            period.setEndDate(LocalDate.parse(string));
-        }
-        return null;
     }
 }
